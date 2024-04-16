d7c63da7d436016492287a218b8cdfb6cd62990e
dudkolau <dudkolau@fel.cvut.cz>
2024-04-16 20:10:55 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module cz.cvut.fel.pjv.dudkolau {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens cz.cvut.fel.pjv.dudkolau to javafx.fxml;
+    exports cz.cvut.fel.pjv.dudkolau;
+}
