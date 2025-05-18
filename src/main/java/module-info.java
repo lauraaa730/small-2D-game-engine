f4f035dd2cc6f8cb2c554ca90ca88bb6e7cbd75a
Laura Kateřina Dudková <dudkolau@fel.cvut.cz>
2025-05-18 23:26:36 +0200

    Deleted redundant dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,9 @@
 module cz.cvut.fel.pjv.dudkolau {
     requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
 
-    opens cz.cvut.fel.pjv.dudkolau to javafx.fxml;
     opens cz.cvut.fel.pjv.dudkolau.Model to com.fasterxml.jackson.databind;
 
     exports cz.cvut.fel.pjv.dudkolau;
